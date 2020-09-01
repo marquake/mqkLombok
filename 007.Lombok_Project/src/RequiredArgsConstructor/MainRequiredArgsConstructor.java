8e16a20ed0fbbad1f3565195d351991b1995114f
marquake <marcosms@gmail.com>
2020-09-01 12:12:07 +0200

    primera subida de pruebas con el proyecto LOMBOK

---
+++ 007.Lombok_Project/src/RequiredArgsConstructor/MainRequiredArgsConstructor.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package RequiredArgsConstructor;
+
+/**
+ * 
+ *
+ */
+public class MainRequiredArgsConstructor {
+
+	/**
+	 * 
+	 */
+	public MainRequiredArgsConstructor() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Persona persona = new Persona();
+
+//		persona.setSoloSet("esto es solo set");
+//
+//		System.out.println("pgs.getSoloGet(): " + pgs.getSoloGet());
+//
+//		System.out.println("pgs.datoNoAlcanzableConGetSet: " + pgs.datoNoAlcanzableConGetSet);
+//		
+//		persona.setVariable_Get_Set("variable get y set");
+//
+//		System.out.println("pgs.getVariable_Get_Set(): " + persona.getVariable_Get_Set() );
+//		
+	}
+
+}
+
+class Persona{
+	
+	public String nombre;
+	public int edad;
+	
+}
